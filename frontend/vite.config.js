95705060c31b37401a0e07f29095c7147239a483
SaraRadulovic003 <radulovics003@gmail.com>
2026-02-18 01:29:10 +0100

    Cart tests

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setupTests.js'],
+    globals: true,
+    restoreMocks: true,
+    clearMocks: true,
+    mockReset: true,
+  },
 });
