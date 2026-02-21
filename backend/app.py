5ff645508f649a33c7508739ef5203f82debc35b
SaraRadulovic003 <radulovics003@gmail.com>
2026-02-21 01:38:17 +0100

    Docker

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,13 @@
+import os
+from dotenv import load_dotenv
+
 from app import create_app
+
+load_dotenv()
 
 app = create_app()
 
+host = "0.0.0.0" if os.getenv("LOCAL_OR_DOCKER", "local") == "docker" else "127.0.0.1"
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host=host, port=5000)
